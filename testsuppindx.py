1a99d0487833cbbb740728ca6cc688d4ffd3e072
paulreastham <57000046+paulreastham@users.noreply.github.com>
2026-05-28 18:25:54 +0100

    Fixed test code so now works as in earlier commit.
    Error was that normalization error is dependent on step so
    trace needs rescaling separately for each step.

--- testsuppindx.py
+++ testsuppindx.py
@@ -48,11 +48,9 @@ ptt=oqupy.PtTempo(bath=bath,
             extra_dim=False)
 
 ptt.compute()
-#%% 
+
 pttempotest=ptt.get_process_tensor()
 print('MPO dimensions ',pttempotest.get_mpo_tensor(2).shape)
-
-#%% 
 
 ptt2=oqupy.PtTempo(bath=bath,
             start_time=0,
@@ -61,16 +59,44 @@ ptt2=oqupy.PtTempo(bath=bath,
             extra_dim=True)
 ptt2.compute()
 pttempotest2=ptt2.get_process_tensor()
-#%%
+
 print('MPO dimensions ',pttempotest2.get_mpo_tensor(2).shape)
+# %% 
+for step in reversed(range(len(pttempotest2))):
+    print('Step ',step,' Shape ',pttempotest2.get_mpo_tensor(step).shape)
 
-for step in range(len(pttempotest2)):
-    old=pttempotest2.get_mpo_tensor(step)
-    pttempotest2.set_mpo_tensor(step,old[:,:,:-1,:-1])
+# %%
+for step in reversed(range(len(pttempotest2))):
+    print('Step ',step,' Cap shape ',pttempotest2.get_cap_tensor(step).shape)
 
+# %%
+print(pttempotest2.hilbert_space_dimension)
+# %%
+ptorg=pttempotest2
+hsd=ptorg.hilbert_space_dimension
+hsdp=hsd+1
 
+result=oqupy.SimpleProcessTensor(
+    hilbert_space_dimension=ptorg.hilbert_space_dimension,
+    dt=ptorg.dt,
+    transform_in=ptorg.transform_in,
+    transform_out=ptorg.transform_out,
+    name=ptorg.name,
+    description=ptorg.description
+    )
+    
+for step in range(len(ptorg)):
+    result.set_cap_tensor(step,ptorg.get_cap_tensor(step))
+    mpotensor=ptorg.get_mpo_tensor(step,transformed=False)
+    a,b,*_=mpotensor.shape
+    if mpotensor.ndim==4:
+        mpotensor=mpotensor[:,:,:-1,:-1]
+    elif mpotensor.ndim==3:
+        mpotensor=mpotensor[:,:,:-1,:-1]
+    result.set_mpo_tensor(step,mpotensor)      
+result.set_cap_tensor(len(ptorg),ptorg.get_cap_tensor(len(ptorg)))    
 
-#%%
+# %%
 
 inisplit=1.0
 # choose initial state to be thermal equilibrium given hamiltonian inisplit*(sigmax/2)
@@ -93,109 +119,35 @@ dynamicspttest=oqupy.compute_dynamics(
     start_time=0)
 
 dynamicspttest2=oqupy.compute_dynamics(
-    process_tensor=pttempotest2,        
+    process_tensor=result,        
     system=system,
     initial_state=rhoini,
     start_time=0)
 
+# %%
+stateslist=dynamicspttest2._states
+traceslist=np.array(dynamicspttest2._states).trace(axis1=1,axis2=2)
+dynamicspttest2._states=[state/trace for state,trace in zip(stateslist,traceslist)]
+#%% print(dynamicspttest2._states.trace(axis1=1,axis2=2))
+
+# %%
+
 t,sx=dynamicspttest.expectations(op.sigma('x'),real=True)
+
 # renormalize traces 
-# tracefromtest=dynamicspttest2.states[10].trace()
+#tracefromtest=dynamicspttest2.states[10].trace()
 #dynamicspttest2._states=dynamicspttest2._states/tracefromtest
 
 t2,sx2=dynamicspttest2.expectations(op.sigma('x'),real=True)
 
+
+
 fig,ax=plt.subplots(1)
 ax.plot(t,sx,'o',label='Standard')
-#ax.plot(t2,sx2,label='Fancy caps')
+ax.plot(t2,sx2,label='Fancy caps')
 #ax.set_ylim(-1,1)
 ax.legend()
 plt.show()
-
-
-# %%
-# let's try the other way
-
-padcoupling=np.pad(op.sigma("z")/2.0,((0,1),(0,1)),constant_values=0.0)
-padbath=oqupy.Bath(padcoupling, correlations)
-
-# %%
-
-ptt3=oqupy.PtTempo(bath=padbath,
-            start_time=0,
-            end_time=tf,
-            parameters=parameters,
-            extra_dim=False)
-ptt3.compute()
-pttempotest3=ptt3.get_process_tensor()
-# %%
-hampad=np.pad(inisplit*op.sigma('x')/2,((0,1),(0,1)),constant_values=0.0)
-system3=oqupy.System(hampad)
-rhoinipad=np.pad(rhoini,((0,1),(0,1)),constant_values=0.0)
-
-
-dynamicspttest3=oqupy.compute_dynamics(
-    process_tensor=pttempotest3,        
-    system=system3,
-    initial_state=rhoinipad,
-    start_time=0)
-
-
-# %%
-padsigx=np.pad(op.sigma('x'),((0,1),(0,1)),constant_values=0.0)
-t3,sx3=dynamicspttest3.expectations(padsigx,real=True)
-
-plt.plot(t3,sx3)
-# %%
-
-# construct an expanded process tensor with an extra null state
-# as a way of making PTTEMPO trace caps work 
-
-def expand_op(op):
-    return np.pad(op,((0,1),(0,1)),constant_values=0.0))
-
-def compute_pt_extradim(coupling_operator,correlations,
-                        start_time,
-                        end_time,
-                        parameters):
-    coupling_pad=expand_op(coupling)
-    padbath=oqupy.bath(coupling_pad,correlations)
-    ptt=oqupy.PtTempo(bath=padbath,
-            start_time=start_time,
-            end_time=end_time,
-            parameters=parameters)
-    ptt.compute() 
-    return ptt
-
-# take the process tensor object for such an expanded Hilbert space and compute the caps
-def compute_caps_augdof(pt):
-    self=pt
-    length = len(self)
-    caps = [np.array([1.0], dtype=NpDtype)]
-    last_cap = tn.Node(caps[-1])
-    slicing_cap=np.zeros(self._hs_dim**2)
-    slicing_cap[-1]=1.0#/self._hs_dim
-
-    for step in reversed(range(length)):
-        trace_square = tn.Node(slicing_cap)
-        trace_in = tn.Node(self._trace_in)
-        trace_out = tn.Node(self._trace_out)
-        ten = tn.Node(self._mpo_tensors[step])
-
-        if len(ten.shape) == 3:
-            ten[1] ^ last_cap[0]
-            ten[2] ^ trace_square[0]
-            new_cap = ten @ last_cap @ trace_square
-        else:
-            ten[1] ^ last_cap[0]
-            ten[2] ^ trace_in[0]
-            ten[3] ^ trace_out[0]
-            new_cap = ten @ last_cap @ trace_in @ trace_out
-        caps.insert(0, new_cap.get_tensor())
-        last_cap = new_cap
-        
-    return caps
-
 
 
 # %%
