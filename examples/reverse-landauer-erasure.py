16b88613d2a34bd011582fa6f8644995224a3cbe
paulreastham <57000046+paulreastham@users.noreply.github.com>
2026-06-02 18:38:32 +0100

    More work on the example/Eoin's problem of reverse Landauer

--- examples/reverse-landauer-erasure.py
+++ examples/reverse-landauer-erasure.py
@@ -23,6 +23,40 @@ from scipy.integrate import solve_ivp
 from scipy.interpolate import interp1d
 from scipy.optimize import minimize,Bounds
 
+from wolframclient.evaluation import WolframLanguageSession
+from wolframclient.language import wl, wlexpr
+session = WolframLanguageSession('/Applications/Wolfram.app/Contents/MacOS/WolframKernel')
+
+session.evaluate("""
+elsol[inispl_?NumericQ,betaomegac_?NumericQ]:=Module[{eqns},
+eqns=1/(2 \[Omega]c r[t]^2) E^(r[t]/\[Omega]c) Sech[1/2 \[Beta] r[t]]^4 (\[Omega]c Sinh[\[Beta] r[t]] Derivative[1][r][t]^2+r[t] ((-2 \[Beta] \[Omega]c+\[Beta] \[Omega]c Cosh[\[Beta] r[t]]-Sinh[\[Beta] r[t]]) Derivative[1][r][t]^2-2 \[Omega]c Sinh[\[Beta] r[t]] Derivative[2][r][t])) /. {\[Beta]->1,\[Omega]c->betaomegac}; (* Solve the equations in dimensionless form *);
+res[inisl_?NumericQ]:=r /. NDSolve[{eqns==0,r[0]==inispl,r'[0]==inisl},r,{t,0,1}][[1]];
+solvedforisl=a /. FindRoot[res[a][1]==0,{a,0}];
+res[solvedforisl]];
+optimaltdlensol[temp_, inisplit_, tf_, omegac_, times_] := Module[
+  {
+    t = N[temp],
+    ini = N[inisplit],
+    duration = N[tf],
+    oc = N[omegac],
+    ts,
+    dimensionlessresult
+  },
+
+  (* Force times into a numeric list *)
+  ts = N @ Flatten @ {times};
+
+  (* Solve in dimensionless units *)
+  dimensionlessresult = elsol[ini/t, oc/t];
+
+  (* Evaluate numerically at each time *)
+  Developer`ToPackedArray[
+    t * (N[dimensionlessresult[#/duration]]) & /@ ts
+  ]
+]
+""")
+
+
 pt_parameters = {'epsrel':10**(-7),
                  'alpha':0.1,
                  'omega_cutoff':1,                 
@@ -36,6 +70,7 @@ epsrel = pt_parameters['epsrel']
 dt=pt_parameters['dt']
 # we will consider dynamics up to tf
 # tfswitch is the end time of the smooth switching protocol
+# %%
 tf=100
 tfswitch=20
 # spectral density
@@ -72,19 +107,28 @@ rhoini=oqupy.operators.spin_dm('x+')*pupx+oqupy.operators.spin_dm('x-')*pdnx
 system=oqupy.System(inisplit*op.sigma('x')/2)
 # parameterizedsystem with time-dependent splitting spl
 
+# %%
 def hamiltonian(spl):
      return op.sigma('x')*spl/2
 
 system = oqupy.ParameterizedSystem(hamiltonian)
 
-halfsteptimes=(dt/4)+np.arange(num_steps*2)*dt/2
-tdsplit=np.ones(num_steps*2) # simple test
+halfsteptimes=list((dt/4)+np.arange(num_steps*2)*dt/2)
+
+# %%
+tdsplit=session.evaluate(wl.Global.optimaltdlensol(temperature,inisplit,tf,omega_cutoff,halfsteptimes))
+# session.terminate()
+# %%
+
+#tdsplit=np.ones(num_steps*2) # simple test
+tdsplit=np.array(tdsplit)
 tdsplit=tdsplit.reshape((num_steps*2,1))
 
 dynamics=oqupy.state_gradient(system,rhoini,[],[ttipt],tdsplit,start_time=0.0,num_steps=num_steps,dynamics_only=True)
 
 dynamicscf = oqupy.state_gradient(system,rhoini,[],[ttiheatpt],tdsplit,start_time=0.0,num_steps=num_steps,dynamics_only=True)
 
+t,s_x=dynamics.expectations(op.sigma('x'),real=True)
 heats=dynamicscf.states.trace(axis1=1,axis2=2).imag/u
 
 fig,axs=plt.subplots(3)
@@ -97,6 +141,8 @@ axs[1].set_xlabel(r'$t$')
 axs[1].set_ylabel(r'$\langle \sigma_x \rangle$')
 axs[1].set_ylim(-1.0,0.0)
 axs[2].plot(halfsteptimes,tdsplit)
+axs[2].set_xlabel(r'$t$')
+axs[2].set_ylabel(r'$\omega_q(t)$')
 plt.legend()
 plt.show()
 
@@ -107,6 +153,8 @@ lamconst=2
 def smswitch(t):
     if t<=tfswitch:
         return (0.1+0.9*(t**lamconst/(t**(lamconst)+(tfswitch-t)**(lamconst))))
+    elif t>=(tf-tfswitch):
+        return 0.1+0.9*((tf-t)**lamconst/((tf-t)**lamconst+(tf-t-tfswitch)**(lamconst)))
     else:
         return 1.0
 smv=np.vectorize(smswitch)
@@ -114,139 +162,47 @@ num_steps=int(tf/dt)
 alpha_tramp=smv(np.arange(num_steps)*dt)
 
 # %%
-pttempotest=oqupy.pt_tempo_compute(bath=bath,
-                             start_time=0,
-                             end_time=50,
-                             parameters=parameters)
-pttempotestswitch=oqupy.pt_tempo_compute(bath=bath,
-                             start_time=0,
-                             end_time=50,
-                             parameters=parameters,
-                             alpha_t=alpha_tramp)
+
+ptt=oqupy.PtTempo(bath=bath,
+        start_time=0,
+        end_time=tf,
+        parameters=parameters,
+        alpha_t=alpha_tramp)
+
+ptswitch=ptt.get_process_tensor()
+
+ptth=oqupy.PtTempo(bath=bathcf,
+                    start_time=0,
+                    end_time=tf,
+                    parameters=parameters,
+                    alpha_t=alpha_tramp,
+                    extra_dim=True)
+
+heatptswitch=ptth.get_process_tensor()
+
+# %%
+
+dynamicssw=oqupy.state_gradient(system,rhoini,[],[ptswitch],tdsplit,start_time=0.0,num_steps=num_steps,dynamics_only=True)
+
+dynamicsswcf = oqupy.state_gradient(system,rhoini,[],[heatptswitch],tdsplit,start_time=0.0,num_steps=num_steps,dynamics_only=True)
 
 
-dynamicspttempo=oqupy.compute_dynamics(
-    process_tensor=pttempotest,        
-    system=system,
-    initial_state=rhoini,
-    start_time=0)
+tsw,sxsw=dynamicssw.expectations(op.sigma('x'),real=True)
+heatssw=dynamicsswcf.states.trace(axis1=1,axis2=2).imag/u
 
-dynamicspttemposwitch=oqupy.compute_dynamics(
-    process_tensor=pttempotestswitch,        
-    system=system,
-    initial_state=rhoini,
-    start_time=0)
+fig,axs=plt.subplots(3)
 
-dynamicsttitempo=oqupy.compute_dynamics(
-    process_tensor=ttitest,        
-    system=system,
-    initial_state=rhoini,
-    start_time=0,
-    num_steps=num_steps)
-
-
-t,sx=dynamicspttempo.expectations(op.sigma('x'),real=True)
-t2,sx2=dynamicspttemposwitch.expectations(op.sigma('x'),real=True)
-t3,sx3=dynamicsttitempo.expectations(op.sigma('x'),real=True)
-
-poldp=spi.quad(lambda x: correlations.spectral_density(x)/(4.0*(x+inisplit)**2),0,30)[0]
-polpred=-np.exp(-2*poldp)
-
-fig,ax=plt.subplots(1)
-ax.plot(t,sx,'o',label='PT-TEMPO, time-indep coupling')
-ax.plot(t2,sx2,label='PT-TEMPO, switched coupling')
-ax.plot(t3,sx3,label='TTI-TEMPO, time-indep coupling')
-ax.axhline(polpred,label='Polaron result')
-ax.legend()
+axs[0].plot(t,heatssw)
+axs[0].set_xlabel(r'$t$')
+axs[0].set_ylabel(r'$\langle Q \rangle$')
+axs[1].plot(t,sxsw)
+axs[1].set_xlabel(r'$t$')
+axs[1].set_ylabel(r'$\langle \sigma_x \rangle$')
+axs[1].set_ylim(-1.0,0.0)
+axs[2].plot(halfsteptimes,tdsplit)
+axs[2].set_xlabel(r'$t$')
+axs[2].set_ylabel(r'$\omega_q(t)$')
+plt.legend()
 plt.show()
-
-# %%
-# Heat dynamics
-# To get correct heats from PT-TEMPO at intermediate times
-# need the extra_dim=True to get the correct caps.
-
-pttempotestheat=oqupy.pt_tempo_compute(bath=bathcf,
-                             start_time=0,
-                             end_time=tf,
-                             parameters=parameters,
-                             extra_dim=True)
-
-pttempotestheatswitch=oqupy.pt_tempo_compute(bath=bathcf,
-                             start_time=0,
-                             end_time=tf,
-                             parameters=parameters,
-                             alpha_t=alpha_tramp,
-                             extra_dim=True)
-
-heatdynamicstti=oqupy.compute_dynamics(
-    process_tensor=ttiheattest,        
-    system=system,
-    initial_state=rhoini,
-    start_time=0,
-    num_steps=num_steps)
-
-heatdynamicspttest=oqupy.compute_dynamics(
-    process_tensor=pttempotestheat,        
-    system=system,
-    initial_state=rhoini,
-    start_time=0)
-heatdynamicspttestswitch=oqupy.compute_dynamics(
-    process_tensor=pttempotestheatswitch,        
-    system=system,
-    initial_state=rhoini,
-    start_time=0)
-# %%
-# Extract heats and plot
-
-heatspt=heatdynamicspttest.states.trace(axis1=1,axis2=2).imag/u
-heatsswitch=heatdynamicspttestswitch.states.trace(axis1=1,axis2=2).imag/u
-heatstti=heatdynamicstti.states.trace(axis1=1,axis2=2).imag/u
-
-fig,ax=plt.subplots(1)
-polht=spi.quad(lambda x: correlations.spectral_density(x)*x/(4.0*(x+inisplit)**2),0,30)[0] # heat (environment energy) in polaron state
-ax.plot(heatdynamicspttest._times,heatspt,label='PT-TEMPO, time-indep coupling')
-ax.plot(heatdynamicspttestswitch._times,heatsswitch,label='PT-TEMPO, switched coupling')
-ax.plot(heatdynamicstti._times,heatstti,'x',label='TTI-TEMPO, time-indep coupling')
-ax.axhline(polht,label='Polaron result')
-ax.set_ylim(0,0.05)
-ax.legend()
-plt.show()
-
-# %%
-runtempo=True
-if runtempo:
-    temporesramp=oqupy.tempo_compute(system=system,
-                                bath=bath,
-                                initial_state=rhoini,
-                                start_time=0,
-                                end_time=10, #tf,
-                                alpha_t=alpha_tramp,
-                                parameters=parameters)
-    t4,sx4=temporesramp.expectations(op.sigma('x'),real=True)
-
-    fig,ax=plt.subplots(1)
-    ax.plot(t2,sx2,'o',label='PT-TEMPO, switched coupling')
-    ax.plot(t4,sx4,label='TEMPO, switched coupling')
-    ax.legend()
-    plt.show()
-
-# %%
-runtempoheats=True
-if runtempoheats:
-    temporesheatramp=oqupy.tempo_compute(system=system,bath=bathcf,
-                                initial_state=rhoini,
-                                start_time=0,
-                                end_time=10, #tf,
-                                parameters=parameters,
-                                alpha_t=alpha_tramp)
-
-    tempoheatsramp=temporesheatramp.states.trace(axis1=1,axis2=2).imag/u
-
-    fig,ax=plt.subplots(1)
-    ax.plot(heatdynamicspttestswitch._times,heatsswitch,label='PT-TEMPO, switched coupling')
-    ax.plot(temporesheatramp._times,tempoheatsramp,label='TEMPO, switched coupling')
-    ax.set_ylim(0,0.05)
-    ax.legend()
-    plt.show()
 
 # %%
